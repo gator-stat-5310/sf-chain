649011f8d511354d8f9494c1efc6ca2841188e34
surj1@gator.uhd.edu <surj1@gator.uhd.edu>
2021-03-21 16:35:31 -0500

    block implementation

---
+++ block.js
@@ -0,0 +1,17 @@
+class Block {
+    constructor(timeStamp, lastHash, hash, data) {
+        this.timeStamp = timeStamp;
+        this.lastHash = lastHash;
+        this.hash = hash;
+        this.data = data;
+    } 
+    toString() {
+    `Block-
+    TimeStamp: ${this.timeStamp}
+    LastHash : ${this.lastHash.substring(0,10)}
+    Hash     : ${this.hash.substring(0,10)}
+    Data     : ${this.data}`;
+    }
+}
+
+module.exports = Block;
