b17fb5b82be9019f4345e722f43faebab0d3e441
surj1@gator.uhd.edu <surj1@gator.uhd.edu>
2021-03-25 22:07:57 -0500

    local changes

--- block.js
+++ block.js
@@ -1,12 +1,13 @@
 class Block {
     constructor(timeStamp, lastHash, hash, data) {
-        this.timeStamp = timeStamp;
-        this.lastHash = lastHash;
-        this.hash = hash;
-        this.data = data;
+        this.timeStamp = timeStamp; //current time
+        this.lastHash = lastHash; //last hash of the block
+        this.hash = hash; // hash_fn(data+last_hash)
+        this.data = data; // real information
     } 
+
     toString() {
-    `Block-
+    return `Block -
     TimeStamp: ${this.timeStamp}
     LastHash : ${this.lastHash.substring(0,10)}
     Hash     : ${this.hash.substring(0,10)}
